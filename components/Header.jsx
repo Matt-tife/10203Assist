ec0ac7b35068212c4801092e2bccd85bde2a40e3
Auxtnn <119543074+Auxtnn@users.noreply.github.com>
2023-08-21 12:06:08 -0700

    initial

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className='sicky top-0 w-full h-[10vh] bg-blue-600'>
-      header
+    <header className="header sticky top-0 z-50 w-full bg-snow flex justify-between items-center px-6 md:w-full">
+      <h1 className="text-4xl font-bold">
+        AssistivTeQ<span className="text-6xl text-blue">.</span>
+      </h1>
+      <nav className="hidden md:flex md:items-center">
+        <ul className="flex space-x-6">
+          <li className="hover:text-blue font-bold transition">
+            <Link href="/about">About Us</Link>
+          </li>
+          <li className="hover:text-blue font-bold transition">
+            <Link href="/services">Services</Link>
+          </li>
+          <li className="hover:text-blue font-bold transition">
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/contact" className="hover:text-blue font-bold rounded-3xl py-2 px-8 border-teal-400 border-solid border-2 transition-all">SignUp</Link>
+          </li>
+          <li>
+            <Link href="/contact" className="hover:bg-teal-400 font-bold rounded-3xl py-2 px-8 bg-blue text-white transition-all">Login</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ components/ServiceSection.jsx
@@ -0,0 +1,237 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section class="w-full">
+      <div class="container mx-auto">
+      
+        <div class="text-center mx-auto mb-10 max-w-[510px]">
+          <h3 className="font-semibold text-center text-lg text-primary mb-2 block text-blue">
+            Our Services
+          </h3>
+          <h2 className="font-bold text-3xl sm:text-4xl md:text-5xl mb-4 text-center text-blue">
+            What We Offer
+          </h2>
+          <p className="text-base">
+            Our evidence-based approaches ensure that you receive the highest
+            quality care in a safe and confidential environment.
+          </p>
+        </div>
+        {/* </div>
+        </div> */}
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+              pb-8
+              px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Solution Focused Therapy
+              </h4>
+              <p>
+                Rather than dwelling on problems, our therapists guide you
+                toward identifying your goals and finding strategies to achieve
+                them to be successful.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+               pb-8
+               px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Marital and Family Therapy
+              </h4>
+              <p>
+                We are always greared dedicated to help couples and also
+                families navigate challenges, resolve conflicts, and build
+                healthy relationships.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+               pb-8
+               px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Anxiety and Stress Management
+              </h4>
+              <p>
+                Our expert therapists are always here to provide you with
+                practical tools, strategies and guidelines for a calmer and more
+                balanced life.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+               pb-8
+               px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Cognitive Behaviour Therapy
+              </h4>
+              <p>
+                Collaborating with our therapists, you'll learn practical
+                strategies and schedules to manage mental health conditions such
+                as depression and anxiety.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+               pb-8
+               px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Parent Education Training
+              </h4>
+              <p>
+                Our experts provide valuable insights to enhance your parenting
+                skills and create a nurturing family environment for you and
+                your own.
+              </p>
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+            <div
+              className="
+               pb-8
+               px-6
+               md:px-7
+               xl:px-10
+               rounded-[20px]
+               bg-snow
+               shadow-md
+               hover:shadow-lg
+               mb-8
+               "
+            >
+              <div
+                className="
+                  w-[70px]
+                  h-[70px]
+                  flex
+                  items-center
+                  justify-center
+                  rounded-2xl
+                  
+                  "
+              ></div>
+              <h4 className="font-semibold text-xl text-blue mb-3">
+                Career Transition Counseling
+              </h4>
+              <p>
+                We provide guidance and support for individuals seeking to make
+                significant changes in their professional lives and beyond.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
